698c9786d51afd4b6e5efef874429c3f1bc3297b
Liron <liron.waisblat@gmail.com>
2022-12-11 22:26:58 +0200

    init commit

--- settings.py
+++ settings.py
@@ -4,7 +4,7 @@ __all__ = ("mongo_settings",)
 
 
 class MongoSettings(pydantic.BaseSettings):
-    uri: str = "mongodb://localhost:27017/"
+    uri: str = "mongodb://mongo:27017/"
     database: str = "Storage"
     collection: str = "products"
 
